2deee79f1b6abde9edbe363ce797ece09f410c93
vanshajrana2006 <vanshajrana2006@gmail.com>
2026-09-06 19:32:44 +0000

    Add JavaScript data types basics

---
+++ basics/datatypes.js
@@ -0,0 +1,6 @@
+"use strict";
+// alert("hello")
+console.log(4*9)
+console.log("vanshaj")
+let x="Vanshaj "
+console.log(typeof x)
---
+++ basics/variables.js
@@ -0,0 +1,5 @@
+const accountId=14434
+let acoountEmail="vanshaj@google.com"
+var accountPassword="12345"
+let accountCity="Jaipur"
+console.log(accountId);
