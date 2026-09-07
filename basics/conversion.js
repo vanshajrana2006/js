f5138c4590dd2732b483393b155c3585cc0efd99
vanshajrana2006 <vanshajrana2006@gmail.com>
2026-09-07 18:46:18 +0000

    Updated JavaScript basics

--- basics/conversion.js
+++ basics/conversion.js
@@ -1,5 +1,11 @@
 let score=33
 // const {score}=req.body
-console.log(typeof score)
-let str=String(score)
-console.log(typeof str)
+// console.log(typeof score)
+// let str=String(score)
+// console.log(typeof str) 
+
+console.log("1"+2)
+console.log("1"+"2")
+console.log("1"+2+"1")
+console.log(1+2+"3")
+console.log("2"===2)
